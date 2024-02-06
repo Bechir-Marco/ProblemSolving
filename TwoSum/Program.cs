0cdf88c9f303860c7f0f7852cad1f81be05b58fd
Bechir-Marco <bechir.marko@gmail.com>
2024-02-06 17:27:43 +0100

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            if (!map.ContainsKey(nums[i]))
+            {
+                map.Add(nums[i], i);
+            }
+        }
+        return new int[] { 0, 0 }; 
+    }
+
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+
+        int[] result = p.TwoSum([ 3,2,3,3,33,3,3,3,3,3,3,3,33,0, 2, 4 ], 6);
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine($"Our index [{i}] is {result[i]}");
+
+        }
+
+    }
+}
