f5b71fa9a2de43e3b56d325ac368f9e06f6b9905
Bechir-Marco <bechir.marko@gmail.com>
2024-02-12 11:36:12 +0100

    Move zeros solution c#

---
+++ Move Zeroes (c#)/solution.cs
@@ -0,0 +1,44 @@
+﻿internal class MoveZeroes
+{
+    public void moveZeroes(int[] nums)
+    {
+        int[] arr = new int[nums.Length];
+        int counter = 0;
+        int arrIndex = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0) {
+                counter++;
+
+             }
+            if (nums[i] != 0)
+            {
+                arr[arrIndex] = nums[i];
+                Console.WriteLine($"oki {arr[arrIndex]}");
+                arrIndex++;
+            }
+         
+        }
+
+        for (int j = arrIndex; j < arr.Length  ; j++)
+        {
+            arr[j] = 0;
+        }
+        Array.Copy(arr, 0, nums, 0, arr.Length);
+    }
+
+    private static void Main(string[] args)
+    {
+        MoveZeroes mz = new MoveZeroes();
+        int[] tes = [0, 1, 0, 3, 12];
+
+        mz.moveZeroes(tes);
+
+        for (int i = 0; i < tes.Length; i++)
+        {
+            Console.WriteLine($"Our inaaaadex [{i}] is {tes[i]}");
+
+        };
+    }
+}
--- TwoSum(c#)/twosum.cs
+++ TwoSum(c#)/twosum.cs
@@ -20,17 +20,4 @@ internal class Program
         }
         return new int[] { 0, 0 }; 
     }
-
-    private static void Main(string[] args)
-    {
-        Program p = new Program();
-
-        int[] result = p.TwoSum([ 3,2,3,3,33,3,3,3,3,3,3,3,33,0, 2, 4 ], 6);
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.WriteLine($"Our index [{i}] is {result[i]}");
-
-        }
-
-    }
 }
