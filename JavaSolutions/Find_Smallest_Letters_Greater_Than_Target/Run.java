85f8ea8b5ff6a3d01de38077a4da278ab772d1f5
Bechir-Marco <bechir.marko@gmail.com>
2024-11-05 01:23:16 +0100

    next grated letter binary search

--- JavaSolutions/Find_Smallest_Letters_Greater_Than_Target/Run.java
+++ JavaSolutions/Find_Smallest_Letters_Greater_Than_Target/Run.java
@@ -2,7 +2,10 @@ package Find_Smallest_Letters_Greater_Than_Target;
 
 public class Run {
     public static void main(String[] args) {
-
+        Sol sol = new Sol();
+        char [] chars = {'c','f','j'};
+        char  target = 'c';
+        System.out.println("Resulat: "+sol.nextGreatestLetter(chars,target));
     }
 
 }
--- JavaSolutions/Find_Smallest_Letters_Greater_Than_Target/Sol.java
+++ JavaSolutions/Find_Smallest_Letters_Greater_Than_Target/Sol.java
@@ -2,11 +2,18 @@ package Find_Smallest_Letters_Greater_Than_Target;
 
 public class Sol {
     public char nextGreatestLetter(char[] letters, char target) {
-        for (char c : letters){
-            if (c >target) {
-                return c;
+        int n = letters.length ;
+        int L =0;
+        int R = n-1;
+        if (letters[n-1]<= target || letters[0]>target) return letters[0];
+        while (L+1 <R) {
+            int Mid =L+ ((R-L)/2);
+            if (letters[Mid] <=target) {
+                L=Mid;
+            }else {
+                R=Mid;
             }
         }
-        return letters[0];
+        return letters[R];
     }
 }
