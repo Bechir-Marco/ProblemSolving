cfd03184f55749972e3a17c0f61bb3c9d7fa2b5c
Bechir-Marco <bechir.marko@gmail.com>
2024-11-09 23:48:35 +0100

    listNode cycle two pointers slow and fast with execution for better clearance

--- JavaSolutions/ListNode_Cycle/Run.java
+++ JavaSolutions/ListNode_Cycle/Run.java
@@ -2,6 +2,18 @@ package ListNode_Cycle;
 
 public class Run {
     public static void main(String[] args) {
+    Sol sol = new Sol();
+
+        ListNode nodeA= new ListNode(1);
+        ListNode nodeB= new ListNode(4);
+        ListNode nodeC= new ListNode(3);
+        ListNode nodeD= new ListNode(5);
+        nodeA.next = nodeB;
+        nodeB.next = nodeC;
+        nodeC.next = nodeD;
+        nodeD.next = nodeA;
+
+        System.out.println("cycle est trouvé "+sol.hasCycle(nodeC));
 
     }
 }
--- JavaSolutions/ListNode_Cycle/Sol.java
+++ JavaSolutions/ListNode_Cycle/Sol.java
@@ -2,27 +2,27 @@ package ListNode_Cycle;
 
 public class Sol {
     public boolean hasCycle(ListNode head) {
-        if (head==null || head.next ==null) return false;
+        if (head == null || head.next == null) return false;
 
         ListNode slowP = head;
         ListNode fastP = head;
 
-        while (fastP!= null || fastP.next!=null) {
+        while (fastP != null && fastP.next != null) {
             slowP = slowP.next;
             fastP = fastP.next.next;
-            if (fastP== slowP) return true;
+            if (fastP == slowP) return true;
 
         }
         return false;
     }
+}
 
     class ListNode {
         int val;
-
         ListNode next;
-
         ListNode(int x) {
             val = x;
             next = null;
         }
-}
+    }
+
