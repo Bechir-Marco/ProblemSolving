88300f758ee47eb459822f9dd40fe87348655cb5
Bechir Marco <96595739+Bechir-Marco@users.noreply.github.com>
2025-02-13 15:28:41 +0100

    Time: 1 ms (15.38%), Space: 20.7 MB (7.69%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.php
+++ 0520-detect-capital/0520-detect-capital.php
@@ -5,9 +5,6 @@ class Solution {
      * @return Boolean
      */
   function detectCapitalUse($word) {
-  
-    $word = preg_replace('/\W/', '', $word);
-    if (preg_match('/^[A-Z][a-z]*$/',$word) || preg_match('/^[A-Z]*$/',$word) || preg_match('/^[a-z]*$/',$word)) return true;
-    else return false;
-    }
+    return preg_match('/^[A-Z]+$|^[a-z]+$|^[A-Z][a-z]+$/', $word);
+}
 }
